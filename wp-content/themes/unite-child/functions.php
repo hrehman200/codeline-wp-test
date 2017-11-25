94a6dd2ddfdc606fce68c4a30e760b3be2e79e75
hrehman200 <hrehman200@gmail.com>
2017-11-25 21:44:27 +0500

    Shortcode for latest 5 movies implemented and displayed

--- wp-content/themes/unite-child/functions.php
+++ wp-content/themes/unite-child/functions.php
@@ -11,3 +11,22 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+function getLatestFilms() {
+    $films = get_posts([
+        'post_type' => 'films',
+        'post_status' => 'publish',
+        'numberposts' => 5,
+        'order'    => 'DESC'
+    ]);
+
+    $html = '<ul>';
+
+    foreach($films as $film) {
+        $html .= sprintf('<li><a href="%s">%s</a></li>', get_post_permalink($film->ID), $film->post_title);
+    }
+    $html .= '</ul>';
+
+    return $html;
+}
+add_shortcode('latestfilms', 'getLatestFilms');
